5f72ffb0b5945f1a613a022b43b0bbb15b5a67fc
maozhiqiang <z_q_mao@163.com>
2018-02-24 16:54:57 +0800

    Update preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -1,2 +1,2 @@
-python preprocess.py --num_workers 4 --name ljspeech --in_dir /media/btows/SDB/train_corpus/  --out_dir /media/btows/SDB/train_corpus/wavnet_dataset/ --hparams sample_rate=16000
+python preprocess.py --num_workers 4 --name ljspeech --in_dir train_corpus/  --out_dir train_corpus/wavnet_dataset/ --hparams sample_rate=16000
 
